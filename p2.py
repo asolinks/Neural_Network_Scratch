7d682e29b231ba17a9f68d033a50b455587194ec
c++junkie <asolinks@hotmail.com>
2025-05-21 23:47:27 +0300

    Realignment of documentation giving credit to youtube series watched

--- p2.py
+++ p2.py
@@ -28,18 +28,16 @@ X = [[1, 2, 3, 2.5],
 		   [2.0, 5.0, -1.0, 2.0],
 		   [-1.5, 2.7, 3.3, -0.8]]
 
+'''
 inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
 output =[]
 
 for i in inputs:
-	if (i > 0):
-		output.append(i)
-	elif (i <= 0):
-		output.append(0)
+	output.append(max(0, i))
 
 print(output)
+'''
 
-'''	
 class Layer_Dense:
 	def __init__(self, n_inputs, n_neurons):
 		self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -47,11 +45,17 @@ class Layer_Dense:
 	def forward(self, inputs):
 		self.output = np.dot(inputs, self.weights) + self.biases
 
+class Activation_ReLu:
+	def forward(self, inputs):
+		self.output = np.maximum(0, inputs)
+		
+		
 layer1 = Layer_Dense(4,5)
 layer2 = Layer_Dense(5,2)
 
 layer1.forward(X)
 
 layer2.forward(layer1.output)
+
 print(layer2.output)
-'''
+
