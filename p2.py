e2ea8da589f093defb37dd0a1345e44da4d90883
c++junkie <asolinks@hotmail.com>
2025-05-21 23:13:50 +0300

    1st draft implementation of ReLU activation

--- p2.py
+++ p2.py
@@ -27,7 +27,19 @@ print(layer2_outputs)
 X = [[1, 2, 3, 2.5],
 		   [2.0, 5.0, -1.0, 2.0],
 		   [-1.5, 2.7, 3.3, -0.8]]
-	
+
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output =[]
+
+for i in inputs:
+	if (i > 0):
+		output.append(i)
+	elif (i <= 0):
+		output.append(0)
+
+print(output)
+
+'''	
 class Layer_Dense:
 	def __init__(self, n_inputs, n_neurons):
 		self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -42,3 +54,4 @@ layer1.forward(X)
 
 layer2.forward(layer1.output)
 print(layer2.output)
+'''
